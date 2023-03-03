2ae846e9edb54ec59177fdb6d1d8c5401c093329
Anna Volfman <aniya_07@ukr.net>
2023-03-03 17:42:28 +0200

    add symple typeScript

---
+++ app.js
@@ -0,0 +1,11 @@
+var button = document.querySelector("button");
+var input1 = document.querySelector("#num1");
+var input2 = document.querySelector("#num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    console.log(add(Number(input1.value), Number(input2.value)));
+});
+
+
